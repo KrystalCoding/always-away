0c2794c25b43404ad6bd29038e5a5a807a259d4d
KrystalCoding <krystalcoding@gmail.com>
2023-08-06 20:46:27 +0000

    Adding admin functions

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    summernot_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email_address', 'body')
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
