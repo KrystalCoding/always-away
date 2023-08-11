53d871cb9db81f86ac92563636fff6f3cc663d5e
KrystalCoding <krystalcoding@gmail.com>
2023-08-11 13:35:49 +0000

    Deploying commit

--- alwaysaway/settings.py
+++ alwaysaway/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-krystalcodi-fullstackpr-88zimg00l5o.ws-eu103.gitpod.io', '8000-krystalcodi-fullstackpr-88zimg00l5o.ws-eu102.gitpod.io', 'always-away.herokuapp.com', 'localhost', 'always-away-227aba88ca4e.herokuapp.com']
 
