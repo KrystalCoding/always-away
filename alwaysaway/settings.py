004979aebada1f21d2013c3afe2ee00666fd9fd8
KrystalCoding <krystalcoding@gmail.com>
2023-08-11 13:24:22 +0000

    Created messages to users

--- alwaysaway/settings.py
+++ alwaysaway/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-krystalcodi-fullstackpr-88zimg00l5o.ws-eu102.gitpod.io', 'always-away.herokuapp.com', 'localhost', 'always-away-227aba88ca4e.herokuapp.com']
+ALLOWED_HOSTS = ['8000-krystalcodi-fullstackpr-88zimg00l5o.ws-eu103.gitpod.io', '8000-krystalcodi-fullstackpr-88zimg00l5o.ws-eu102.gitpod.io', 'always-away.herokuapp.com', 'localhost', 'always-away-227aba88ca4e.herokuapp.com']
 
 
 # Application definition
@@ -57,6 +58,15 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Set tags to different bootstrap classes to change the colors. Linked to video
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
