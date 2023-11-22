ad5816fdc6b578a176e191be5e70d182881c3a1e
KrystalCoding <krystalcoding@gmail.com>
2023-11-22 22:17:41 +0000

    Replacing test files into different directory

--- alwaysaway/settings.py
+++ alwaysaway/settings.py
@@ -9,6 +9,8 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+if os.path.isfile(os.path.join(BASE_DIR, 'env.py')):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -17,7 +19,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
--- alwaysaway/test_views.py
+++ alwaysaway/test_views.py
@@ -1,3 +1,5 @@
+import unittest
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
