80872997645f5e616eec5d49cb86abbd57ecda10
KrystalCoding <krystalcoding@gmail.com>
2023-12-05 12:46:06 +0000

    Adding favicon from cloudinary to base.html

--- alwaysaway/settings.py
+++ alwaysaway/settings.py
@@ -20,7 +20,7 @@ if os.path.isfile(os.path.join(BASE_DIR, 'env.py')):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
